50413f730d95d323400cdf37ef4e63041a822ec5
twooopark <twooo.park@gmail.com>
2018-06-21 21:49:50 +0900

    ...

--- src/BackTracking/boj_6603/Main.java
+++ src/BackTracking/boj_6603/Main.java
@@ -1,42 +1,46 @@
-package BackTracking.boj_6603;
-import java.util.*;
-public class Main {
-//	static int[] ans;
-//	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		while(true){
-//			int K = sc.nextInt();
-//			if(K==0) break;
-//			int[] A = new int[K];
-//			ans = new int[K];
-//			for(int i=0; i<K; i++)
-//				A[i] = sc.nextInt();
-//			for(int i=0; i<K-5; i++){
-//				ans[0]=A[i];
-//				go(K, A, i);
+//package BackTracking.boj_6603;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.*;
+//public class Main {
+//	public static void main(String[] args) throws NumberFormatException, IOException{
+//
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st;// = new StringTokenizer(br.readLine());
+//		StringBuilder sb = new StringBuilder();
+//		while(true) {
+//			st = new StringTokenizer(br.readLine());
+//			int N = Integer.parseInt(st.nextToken());
+//			if(N == 0) System.exit(0);
+//			
+//			int[] A = new int[N+1];
+//			for (int i=0; i<N; i++)
+//	    		A[i] = Integer.parseInt(st.nextToken());
+//			
+//			for(int i=0; i<N-5; i++){
+//				System.out.print(A[i]+" ");
+//				go(N, A, i);
 //			}
 //		}
 //	}
 //
 //	static void go(int n, int[] a, int i) {
 //		if(!check(a,i)) return;
-//		if(i==n-1){
-//			for(int j=0; j<n; j++)
-//				System.out.print(ans[j]+" ");
+//		if(i==n-2){
 //			System.out.println();
 //			return;
 //		}
-//		for(int j=i; j<n-5; j++){
-//			ans[i+1]=a[j];
+//		for(int j=i; j<n; j++){
+//			System.out.print(a[i+1]+" ");
 //			go(n, a, i+1);
 //		}
 //		return;
 //	}
 //
 //	static boolean check(int[] a, int i) {
-//		for(int x=0; x<i; x++){
-//			if(ans[i] <= ans[x]) return false;
-//		}
-//		return true;
+//		if(i<1) return true;
+//		return a[i-1] < a[i];
 //	}
 //}
