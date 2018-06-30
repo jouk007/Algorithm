a640cf42cd3bf8f644239b9d0a8290a691f0f348
twooopark <twooo.park@gmail.com>
2018-07-01 02:57:56 +0900

    dp문제 풀기 시작.... 포도주 문제파생

--- src/DinamicProgramming/boj_2579/Main.java
+++ src/DinamicProgramming/boj_2579/Main.java
@@ -1,29 +1,25 @@
-package DinamicProgramming.boj_2579;
+package DinamicProgramming.boj_2579;		
+
 import java.util.*;
 import java.io.*;
-1111111111111111public class Main {
+public class Main {
 	public static void main(String []args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int A[] = new int[N];
-		int B[][] = new int[N][2];
-		
-		for(int i=0; i<N; i++) 
-			A[i] = Integer.parseInt(br.readLine());
 		int Max = 0;
-		for(int i=1; i<N; i++) {
-			if(i-2 >= 0)// 한 계단 갔을 때
-				B[i][0] = A[i-2]+A[i];
-			// 연속 두 계단 갔을 때 (다음엔 2칸가야함)
-			B[i][1] = A[i-1]+A[i];
-			
-			if( A[i-1] < A[i] ) {
-				Max += A[i];
-				i++;
-			}
-			else
-				Max += A[i-1];
+		int A[] = new int[N+1];
+		int dp[][] = new int[N+1][3];
+		for(int i=1; i<=N; i++)
+			A[i] = Integer.parseInt(br.readLine());
+		
+		dp[1][1] = A[1];
+		for(int i=2; i<=N; i++){
+			dp[i][0] = Math.max(dp[i-1][1], dp[i-1][2]);//안밟는다. + A[i]; 
+			dp[i][1] = dp[i-1][0] + A[i];
+			dp[i][2] = dp[i-1][1] + A[i];
 		}
+		//마지막은 반드시 밟아야 하므로 dp[N][0]은 제외한다.
+		Max = Math.max(dp[N][1], dp[N][2]);
 		System.out.println(Max);
 	}
 }
---
+++ src/Sort/basic/boj_2751/Main.java
@@ -0,0 +1,25 @@
+package Sort.basic.boj_2751;
+
+import java.util.*;
+import java.io.*;
+```````````````public class Main {
+	public static void main(String []args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long N = Long.parseLong(br.readLine());
+		if(N==1L) {
+			System.out.println(1);
+			System.exit(0);
+		}
+		int j = 1;
+		long M[] = new long[20000];
+		for(long i=1; i<=1000000050; i=i+6*(j++))  
+			M[j] = i;
+		
+		for(j=0; j<M.length; j++) {
+			if(M[j] > N) {
+				System.out.println(j);
+				break;
+			}
+		}
+	}
+}
